f034fb8b9c97aaffc4f1c08ddea4fa4b4c180b7c
shlok007 <shlok.srivastava15@gmail.com>
2016-03-19 07:20:51 -0400

    controllers now respond to JSON

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions',registrations: 'registrations'}
   get 'users/sign_in' ,:as => :sign_in
   get 'navs/index'
 
